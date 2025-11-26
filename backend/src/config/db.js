62878890f6c3e587d3f5dfb705c984656ac56cdd
Hephzibah Ofomi <h.ofomi@alustudent.com>
2025-11-26 02:38:29 +0200

    database configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
+    password: 'Godislife1*',  
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
 });
 
